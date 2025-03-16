059f7c8a5a2566192a475b5953dea55febeaf550
ANKIT-is-here <ankitpal862005@gmail.com>
2025-03-16 22:45:41 +0530

    Updated DJ_Night component with optimized image styling

--- src/components/DJ_Night.jsx
+++ src/components/DJ_Night.jsx
@@ -16,7 +16,21 @@ export default function DJ_Night(){
 <div className="container my-2 text-center d-flex justify-content-center flex-column align-items-center">
           <div className="row row-cols-1 row-cols-md-1 g-4 my-2 animated slideInRight">
               <div className="card">
-                <img src="..\img\event\dj_night.png" className="card-img-top" alt="" />
+                {/* <img src="..\img\event\dj_night.png" className="card-img-top" alt="" /> */}
+                <img 
+              src="..\img\event\dj_night.png" 
+              alt="DJ Night" 
+              style={{ 
+                maxWidth: "100%", 
+                maxHeight: "700px", 
+                height: "auto", 
+                display: "block", 
+                border: "none",
+                padding: "0",
+                margin: "0",
+                background: "transparent",
+              }} 
+            />
               </div>
             </div>
           </div>
